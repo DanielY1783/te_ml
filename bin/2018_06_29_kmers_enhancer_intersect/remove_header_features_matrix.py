baade2d563af8846958c6040d2d8ecf342188c3e
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-06-29 10:46:56 -0500

    Fixed syntax error and worked on histogram

--- bin/2018_06_29_kmers_enhancer_intersect/remove_header_features_matrix.py
+++ bin/2018_06_29_kmers_enhancer_intersect/remove_header_features_matrix.py
@@ -31,7 +31,7 @@ def main():
     # Store header names to file.
     with open(DIRECTORY + "data/2018_06_29_kmers_enhancers_intersect/header_names.txt", mode = "a") as file:
         for i in range(len(features_df.columns)):
-            features_df.write("Column " + str(i) + ": " + features_df.columns[i])
+            file.write("Column " + str(i) + ": " + features_df.columns[i])
         
     
     features_df.to_csv(DIRECTORY + "data/2018_06_29_kmers_enhancers_intersect/no_header_hervs_kmers_features_matrix.tsv"
