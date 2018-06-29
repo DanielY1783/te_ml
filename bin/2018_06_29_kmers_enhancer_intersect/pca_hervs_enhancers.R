c099383a83211199c32c29c0ecc6f31bd64bace8
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-06-29 14:27:34 -0500

    Edited documentation

--- bin/2018_06_29_kmers_enhancer_intersect/pca_hervs_enhancers.R
+++ bin/2018_06_29_kmers_enhancer_intersect/pca_hervs_enhancers.R
@@ -6,8 +6,10 @@ library("factoextra")
 DIRECTORY = "/dors/capra_lab/users/yand1/te_ml/"
 DATE_DIR = "2018_06_29_kmers_enhancers_intersect/"
 DATA_FILE = "reformatted_hervs_kmers_intersect_enhancers.tsv"
+ENHANCER = 4099 # Column showing enhancer overlap
 
 # Read in input file as data frame.
 hervs_df = read.table(paste(DIRECTORY, "/data", DATE_DIR, DATA_FILE))
 
 # Generate PCA, but leave out the enhancer column as classifier
+hervs_pca <-
