aadcb9df2bb2abee9fa0df823dace31123cdc303
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-06-29 14:43:57 -0500

    pca analysis

--- bin/2018_06_29_kmers_enhancer_intersect/pca_hervs_enhancers.R
+++ bin/2018_06_29_kmers_enhancer_intersect/pca_hervs_enhancers.R
@@ -9,7 +9,25 @@ DATA_FILE = "reformatted_hervs_kmers_intersect_enhancers.tsv"
 ENHANCER = 4099 # Column showing enhancer overlap
 
 # Read in input file as data frame.
-hervs_df = read.table(paste(DIRECTORY, "/data", DATE_DIR, DATA_FILE))
+hervs_df = read.table(paste(DIRECTORY, "/data", DATE_DIR, DATA_FILE, sep = ""))
 
-# Generate PCA, but leave out the enhancer column as classifier
-hervs_pca <-
+# Save to png file
+png_file_name <-
+  paste(DIRECTORY, "/data", DATE_DIR, "pca.png", sep = "")
+png(
+  filename = png_file_name,
+  width = 100,
+  height = 100,
+  res = 100
+)
+
+# Generate PCA, but leave out the enhancer column as classifier. Keep in 2 dimensions for graphing.
+hervs_pca <- PCA(hervs_df[,-ENHANCER], graph = FALSE, ncp = 2)
+print(fviz_pca_ind(hervs_pca,
+             geom.ind = "point", # Show points
+             col.ind = hervs_df$enhancer, # Color by presence or absence of enhancers.
+             legend.title = "Enhancer Overlap"
+             )
+)
+# Close png file
+dev.off()
