9b8f31eca62106fedb0d14a4b35bf45456bca5ac
Daniel Yan <yand1@chgr2.(none)>
2018-07-02 10:57:04 -0500

    Fixed problems related to accre cluster and use full file

--- bin/2018_07_02_kmers_pca_rf/pca_hervs_enhancers.R
+++ bin/2018_07_02_kmers_pca_rf/pca_hervs_enhancers.R
@@ -1,10 +1,11 @@
 # Libaries
+.libPaths("~/R/rlib-3.4.3") # Pick up installed libraries on ACCRE
 library("FactoMineR")
 library("factoextra")
 
 # Class constants
 DIRECTORY <- "/dors/capra_lab/users/yand1/te_ml/"
-DATA_FILE <- "test.tsv"
+DATA_FILE <- "reformatted_hervs_kmers_enhancers_intersect.tsv"
 ENHANCER <- 4099 # Column showing enhancer overlap
 CHROMOSOME <- 0 # Column of chromosome of HERV
 START <- 1 # Column of start location of HERV
@@ -18,8 +19,9 @@ hervs_df <- read.table(paste(DIRECTORY, "data/2018_06_29_kmers_enhancers_interse
 # Save to png file
 png_file_name <-
   paste(DIRECTORY, "results/2018_07_02_kmers_pca_rf/", "pca.png", sep = "")
+# Use type as cairo to avoid X11 display error
 png(
-  filename = png_file_name
+  filename = png_file_name, type = "cairo"
 )
 
 # Generate PCA, but leave out the enhancer column as classifier. Keep in 2 dimensions for graphing.
