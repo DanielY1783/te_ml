f6652e6bfe899100f17e4b0542b84d0495234456
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-05 15:54:06 -0500

    kmer counter for mlt1k

--- bin/2018_07_05_subset_hervs/kmer_counter.py
+++ bin/2018_07_05_subset_hervs/kmer_counter.py
@@ -1,9 +1,10 @@
 
 # coding: utf-8
 
-# Counts k-mers for each herv by reading in file with base pairs for HERVs at /dors/capra_lab/users/yand1/te_ml/data/2018_06_26_kmers_hervs_chromehmm/hervs_hg19.tsv
-# and creating a count for each k-mer and saving to 
-# /dors/capra_lab/users/yand1/te_ml/data/2018_06_27_kmers_hervs_chromehmm/hervs_kmers_features_matrix.tsv
+# New version of 
+# /dors/capra_lab/users/yand1/te_ml/bin/2018_06_28_kmers_faster/accre/kmer_counter_batch
+# for use with data file of mlt1k and enhancer intersect at
+# /dors/capra_lab/users/yand1/te_ml/data/2018_07_05_subset_hervs/pairs_mlt1k_enhancers.tsv
 
 # In[1]:
 
@@ -18,15 +19,15 @@ from itertools import product # For generating all possible 6-mers
 
 # Class constants
 DIRECTORY = "/dors/capra_lab/users/yand1/te_ml/" # Root directory for project
-DATE = "2018_06_27_kmers_hervs_chromehmm/"
-LOCATION = "accre/" # Local machine or accre cluster.
-DATA_FILE = "hervs_hg19.tsv"
-WRITE_FILE = "hervs_kmers_features_matrix.tsv" # File to save results to
+DATE = "2018_07_05_subset_hervs/" # Directory corresponding to date
+DATA_FILE = "pairs_mlt1k_enhancers.tsv"
+WRITE_FILE = "mlt1k_kmers_features_matrix.tsv" # File to save results to
 K = 6 # Length of "k" value for k-mer
 CHR = 0 # Column of chromosome of herv in data file
 START = 1 # Column of start location of herv in data file
 END = 2 # Column of end location of herv in data file
-PAIRS = 3 # Column with string of actual pairs within herv
+ENHANCER = 3 # Column with enhancer presence
+PAIRS = 4 # Column with string of actual pairs within herv
 ALPHABET = "acgt" # Alphabet of base pairs in k-mers
 
 
@@ -50,40 +51,42 @@ def generate_kmers():
 # In[4]:
 
 
-def count_kmers(pairs_df, kmers_list):
-    """Counts the repetitions of each k-mer within each row of the dataframe
-        by using a sliding window and storing results to corresponding column.
+def count_kmers(features_df):
+    """Applies counting repetitions of kmers to the entire data frame.
         
     Args:
-        herv_df(pd.DataFrame): Data frames containing the base pairs for each HERV.
-        kmers_list: List containing strings for all possible k-mers.
+        features_df(pd.DataFrame): Data frames containing the base pairs to be updated.
         
     Return:
-        features_df(pd.DataFrame): New data frame with columns of k-mers updated to 
-            reflect counts within base pairs to use as features matrix.
+        features_df(pd.DataFrame): Data frame with columns of k-mers updated to reflect 
+                                counts within base pairs to use as features matrix.
     """
-    # Add all kmers from kmers_list as columns for new features matrix,
-    # with values default initialized to 0. Drop pairs column.
-    features_df = pairs_df.copy().reindex(columns = (pairs_df.columns.tolist() + kmers_list), 
-                                                      fill_value = 0)
-    features_df = features_df.drop("pairs", axis = "columns")
-    
-    # Iterate over pairs_df to update features_df
-    for index, row in pairs_df.iterrows():
-        # Get base pairs in current row
-        pairs = row["pairs"]
-        
-        # Find k-mers in base pairs using a sliding window and update count in features matrix
-        for i in range(len(pairs) - K):
-            kmer = pairs[i: i + K]
-            # Check that the column exists
-            if kmer in features_df.columns:
-                # Get corresponding column number in features matrix
-                column_number = features_df.columns.get_loc(kmer)
-                # Update features matrix
-                features_df.iloc[index, column_number] += 1
-    
+    features_df = features_df.apply(parse_pairs, axis = "columns")
     return features_df
+
+
+# In[ ]:
+
+
+def parse_pairs(row):
+    """Counts the repetitions of each k-mer within the row.
+    
+    Args:
+        row(pd.Series): Single row representing a HERV to parse for the number of each k-mer.
+
+    Return: 
+        row(pd.Series): Row that is updated with count of each k-mer within the HERV.
+    """
+    # Get base pairs in current row
+    pairs = row[PAIRS]
+    # Find k-mers in base pairs using a sliding window and update count in features matrix
+    for i in range(len(pairs) - K):
+        kmer = pairs[i: i + K]
+        # Check that the column exists
+        if kmer in row:
+            # Update corresponding column number in features matrix
+            row.loc[kmer] += 1
+    return row
 
 
 # In[5]:
@@ -92,10 +95,11 @@ def count_kmers(pairs_df, kmers_list):
 ## Main
 def main():
     # Read in file as pandas dataframe
-    hervs_df = pd.read_table(DIRECTORY + "data/" + "2018_06_26_kmers_hervs_chromehmm/" + DATA_FILE, header = None)
+    hervs_df = pd.read_table(DIRECTORY + "data/" + DATE_DIR + DATA_FILE, header = None)
     
     # Rename columns
-    hervs_df = hervs_df.rename(columns = {CHR: "chr", START: "start", END: "end", PAIRS: "pairs"})
+    hervs_df = hervs_df.rename(columns = {CHR: "chr", START: "start", END: "end", 
+                                          ENHANCER = "enhancer", PAIRS: "pairs"})
     
     # Generate all possible k-mers
     kmers_list = generate_kmers()
@@ -103,7 +107,7 @@ def main():
     # Create features matrix containing count of each k-mer from the base pairs data frame
     features_matrix = count_kmers(pairs_df = hervs_df, kmers_list = kmers_list)
     
-    features_matrix.to_csv(DIRECTORY + "data/" + "2018_06_27_kmers_hervs_chromehmm/" + WRITE_FILE, sep = '\t', index = False)
+    features_matrix.to_csv(DIRECTORY + "data/" + DATE_DIR + WRITE_FILE, sep = '\t', index = False)
 
 
 # In[6]:
