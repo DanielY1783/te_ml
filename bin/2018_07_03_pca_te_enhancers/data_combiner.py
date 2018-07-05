8c22b36ea16046178fb0c3d9db46ae8eb4a8a8f4
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-05 09:24:43 -0500

    Fixed pd.concat parameter

--- bin/2018_07_03_pca_te_enhancers/data_combiner.py
+++ bin/2018_07_03_pca_te_enhancers/data_combiner.py
@@ -61,5 +61,5 @@ if __name__ == "__main__":
         file_list.append("/dors/capra_lab/users/yand1/te_ml/data/2018_07_03_pca_te_enhancers/batch_output/combined_hg19_{}_features_matrix.tsv".format(i))
     combine(file_list = file_list, header = False, 
             output_file = "/dors/capra_lab/users/yand1/te_ml/data/2018_07_03_pca_te_enhancers/combined_features_matrix.tsv",
-            axis = "rows")
+            axis = "index")
 
