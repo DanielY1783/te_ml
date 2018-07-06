af8868acf00bb28de828d19f3349ecaf99be64f6
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-06 13:23:25 -0500

    Worked on converting to module for reusability

--- bin/2018_07_06_genome_shuffle/data_splitter.py
+++ bin/2018_07_06_genome_shuffle/data_splitter.py
@@ -1,7 +1,8 @@
 
 # coding: utf-8
 
-# Modified version of similar file at /dors/capra_lab/users/yand1/te_ml/bin/2018_06_28_kmers_faster/accre
+# Module version of similar file at /dors/capra_lab/users/yand1/te_ml/bin/2018_06_28_kmers_faster/accre
+# Splits data frame into smaller data frames and save to files as necessary.
 
 # In[1]:
 
@@ -14,35 +15,51 @@ import pandas as pd # Data manipulation
 
 
 # Class constants
-DIRECTORY = "/dors/capra_lab/users/yand1/te_ml/" # Root directory for project
-DATE_DIR = "2018_07_06_genome_shuffle/" # Directory related to date
-DATA_FILE = "all_pairs.tsv"
 SPLITS = 100 # Number of files to make
+
+
+# In[ ]:
+
+
+def split_data(df, splits = SPLITS):
+    """Splits dataframe and returns list of new dataframes.
+    
+    Keyword Arguments:
+        df (pd.DataFrame): Data frame to split
+    
+        splits(int): Number of data frames to split into (default = SPLITS).
+        
+    Return:
+        smaller_dfs_list (list): List of the new data frames.
+        
+    """
+    # Create list for return
+    smaller_dfs_list = []
+    
+    # Store number of rows in data file.
+    rows_num = df.shape[0]
+    
+    # Split up files
+    for i in range(SPLITS):
+        start_row = i * (rows_num/SPLITS)
+        end_row = (i + 1) * (rows_num/SPLITS)
+        temp_df = hervs_df.loc[start_row:end_row,]
+        smaller_dfs_list.append(temp_df.copy())
+    return smaller_dfs_list
 
 
 # In[3]:
 
 
-def main():
+if __name__ == "__main__":
     # Read in file as pandas dataframe
-    hervs_df = pd.read_table(DIRECTORY + "data/" + DATE_DIR + DATA_FILE, header = None)
-    
-    # Store number of rows in data file.
-    rows_num = hervs_df.shape[0]
-    
+    hervs_df = pd.read_table("/dors/capra_lab/users/yand1/te_ml/data/2018_07_06_genome_shuffle/all_pairs.tsv", header = None)
+
     # Split data frame into parts.
-    for i in range(SPLITS):
-        start_row = i * (rows_num/SPLITS)
-        end_row = (i + 1) * (rows_num/SPLITS)
-        temp_df = hervs_df.loc[start_row:end_row,]
-        # Save to file
-        temp_df.to_csv(DIRECTORY + "data/" + DATE_DIR + "/batch_input/shuffle_" + str(i) + ".tsv", 
+    df_list = split_data(df = hervs_df)
+    
+    # Save to files
+    for df in df_list:
+        df.to_csv("/dors/capra_lab/users/yand1/te_ml/data/2018_07_06_genome_shuffle/batch_input/shuffle_" + str(i) + ".tsv", 
                        sep = '\t', index = False, header = False)
-
-
-# In[4]:
-
-
-# Call main to run
-main()
 
