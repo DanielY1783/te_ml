c601ed3ee70f4ad2d561d5916a6b371e73311727
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-05 21:52:42 -0500

    Fixed errors

--- bin/2018_07_03_pca_te_enhancers/pca_combined.R
+++ bin/2018_07_03_pca_te_enhancers/pca_combined.R
@@ -6,11 +6,11 @@ library("factoextra")
 # Class constants
 DIRECTORY <- "/dors/capra_lab/users/yand1/te_ml/"
 DATA_FILE <- "combined_features_matrix.tsv"
-CHROMOSOME <- 0 # Column of chromosome of HERV
-START <- 1 # Column of start location of HERV
-END <- 2 # Column of end location of HERV
-LABEL <- 3 # Column labeling as HERV, enhancer, or HERV_enhancer overlap
-PAIRS <- 4 # Column with base pairs
+CHROMOSOME <- 1 # Column of chromosome of HERV
+START <- 2 # Column of start location of HERV
+END <- 3 # Column of end location of HERV
+LABEL <- 4 # Column labeling as HERV, enhancer, or HERV_enhancer overlap
+PAIRS <- 5 # Column with base pairs
 
 # Read in input file as data frame.
 hervs_df <- read.table(paste(DIRECTORY, "data/2018_07_03_pca_te_enhancers/", DATA_FILE, sep = "")
@@ -26,10 +26,11 @@ png(
 )
 
 # Generate PCA, but leave out the enhancer column as classifier. Keep in 2 dimensions for graphing.
-pca_hervs_enhancers <- PCA(hervs_df[-c (CHROMOSOME, START, END, LABEL, PAIRS)], graph = FALSE, ncp = 2)
+pca_df <- subset(hervs_df, select = -c(CHROMOSOME, START, END, LABEL, PAIRS))
+pca_hervs_enhancers <- PCA(pca_df, graph = FALSE, ncp = 2)
 pca_graph <-fviz_pca_ind(pca_hervs_enhancers,
                          geom.ind = "point", # Show points
-                         col.ind = hervs_df$enhancer, # Color by presence or absence of enhancers.
+                         col.ind = hervs_df$label, # Color by presence or absence of enhancers.
                          legend.title = "Enhancer Overlap")
 # Change color palette.
 print(ggpubr::ggpar(pca_graph, palette = "rainbow")
