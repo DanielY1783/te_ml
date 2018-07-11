fc9398e1ec0af6bf08cb2132f6e5a0e18b2ab6a7
DanielY1783 <daniel.yan@vanderbilt.edu>
2018-07-11 15:10:10 -0500

    Travis testing

--- bin/2018_07_11_pca_te_enhancers/test_shuffled_pca.py
+++ bin/2018_07_11_pca_te_enhancers/test_shuffled_pca.py
@@ -10,12 +10,14 @@
 import shuffled_pca as sp
 
 def test_generate_permutations_1():
-    assert sp.generate_components([]) = []
+    assert sp.generate_permutations([]) == []
 
 def test_generate_permutations_2():
-    assert sp.generate_components([1, 2, 3]) = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2,], [2, 3],
-                                               [3, 1], [3, 2], [3, 3]]
+    assert sp.generate_permutations([1, 2, 3]) == [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2,), (2, 3),
+                                               (3, 1), (3, 2), (3, 3)]
 def test_generate_permutations_3():
-    assert sp.generate_components("ab", n = 3) = ["aaa", "aab", "aba", "abb", "baa", "bab",
-                                                 "bba", "bbb"]
+    assert sp.generate_permutations("ab", n = 3) == [('a', 'a', 'a'), ('a', 'a', 'b'), 
+                                                     ('a', 'b', 'a'), ('a', 'b', 'b'), 
+                                                     ('b', 'a', 'a'), ('b', 'a', 'b'),
+                                                     ('b', 'b', 'a'), ('b','b', 'b')]
 
